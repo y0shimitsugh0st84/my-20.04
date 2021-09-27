51686aa269e61723f8d16701e362adcb5170b5ce
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-09-27 23:51:44 +0200

    Create install_tools.sh

---
+++ add_stuff/install_tools.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+### every exit != 0 fails the script
+set -e
+
+echo "Install some common tools for further installation"
+apt-get update 
+apt-get install -y vim wget net-tools locales bzip2 \
+    python-numpy #used for websockify/novnc
+#apt-get -y install git gcc curl make libxml2-utils flex m4 openjdk-8-jdk lib32stdc++6 libelf-dev libssl-dev python-enum34 python-mako syslinux-utils software-properties-common
+apt-get clean -y
+
+echo "generate locales für en_US.UTF-8"
+locale-gen en_US.UTF-8
+
+apt install --fix-missing -y vim wget ca-certificates  pulseaudio xrdp\
+  xauth supervisor uuid-runtime locales \
+  openssh-server sudo git build-essential cmake libuv1-dev uuid-dev libmicrohttpd-dev libssl-dev \
+  nano python-pip  xvfb \
+  python3-pip  locate  libxml2-dev libxslt1-dev libssl-dev libmicrohttpd-dev  \
+  libmysqlclient-dev byobu locate cron python-pyaudio python3-pyaudio ffmpeg \
+  fonts-liberation libappindicator3-1 libfile-basedir-perl libfile-desktopentry-perl libfile-mimeinfo-perl \
+  libindicator3-7  libipc-system-simple-perl libnet-dbus-perl libtie-ixhash-perl libx11-protocol-perl \
+  libxml-parser-perl libxml-twig-perl libxml-xpathengine-perl xdg-utils  xserver-xephyr jq tor xarchiver libavcodec-extra vlc
