63bddc2c3b40f1aab2f2a80f54cb95a688ab2762
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-09-28 02:15:47 +0200

    Update install_tools.sh

--- add_stuff/install_tools.sh
+++ add_stuff/install_tools.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ### every exit != 0 fails the script
 set -e
-
+export DEBIAN_FRONTEND=noninteractive
 echo "Install some common tools for further installation"
 apt-get update 
 apt-get install -y vim wget net-tools locales bzip2 \
